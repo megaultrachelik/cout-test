8d205026b2f61c302c2974fc92749a4043d7c8c9
SusEr <SusEr@WIN-12CKSSBFV79>
2024-08-10 15:33:04 +0600

    Добавьте файлы проекта.

---
+++ cout-test/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	std::cout << "Добро пожаловать в тестовую программу!\nВыберите:\n[1] Message Box  [2] Beep (спикер?)\nВвод: " << std::endl;
+	int menu;
+	std::cin >> menu;
+	switch (menu)
+	{
+		default:
+			std::cout << "Вы ничего не выбрали." << std::endl;
+			break;
+		case 1:
+			MessageBox(0, L"Это MessageBox вызванный через WinAPI.", L"Вау!", 0);
+			break;
+		case 2:
+			Beep(1000, 500);
+			break;
+	}
+}
