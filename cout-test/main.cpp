dda9232779b1fe9aab0c30a8fef0350125879545
megachelik <luwufka0@gmail.com>
2024-08-10 15:35:44 +0600

    меню теперь в цикле, чтобы программа не завершалась.

--- cout-test/main.cpp
+++ cout-test/main.cpp
@@ -4,19 +4,22 @@
 int main()
 {
 	setlocale(LC_ALL, "");
-	std::cout << "Добро пожаловать в тестовую программу!\nВыберите:\n[1] Message Box  [2] Beep (спикер?)\nВвод: " << std::endl;
-	int menu;
-	std::cin >> menu;
-	switch (menu)
+	while (true)
 	{
-		default:
-			std::cout << "Вы ничего не выбрали." << std::endl;
-			break;
-		case 1:
-			MessageBox(0, L"Это MessageBox вызванный через WinAPI.", L"Вау!", 0);
-			break;
-		case 2:
-			Beep(1000, 500);
-			break;
+		std::cout << "\nДобро пожаловать в тестовую программу!\nВыберите:\n[1] Message Box  [2] Beep (спикер?)\nВвод: " << std::endl;
+		int menu;
+		std::cin >> menu;
+		switch (menu)
+		{
+			default:
+				std::cout << "Вы ничего не выбрали." << std::endl;
+				break;
+			case 1:
+				MessageBox(0, L"Это MessageBox вызванный через WinAPI.", L"Вау!", 0);
+				break;
+			case 2:
+				Beep(1000, 500);
+				break;
+		}
 	}
 }
